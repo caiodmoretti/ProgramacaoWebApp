27335616f6791c1828d60bd0ef1d1da402ba55f1
Caio Moretti <102165296+caiodmoretti@users.noreply.github.com>
2023-11-27 21:48:53 -0300

    Adicionando a estrutura do projeto Spring

---
+++ PackNow/src/main/java/com/example/PackNow/controller/apirest/MainRestController.java
@@ -0,0 +1,10 @@
+package com.example.PackNow.controller.apirest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path  = "/api/v1")
+public class MainRestController {
+
+}
